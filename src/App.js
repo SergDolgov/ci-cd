7177c0d80b00db10af253d41fce79663283b2f06
Sergey Dolgov <saddolgov@gmail.com>
2023-12-28 15:55:24 +0200

    inc change

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
 
     return (
         <div className="App">
-            <h1 style={{fontSize: 70, color: 'red'}}>value = {state}</h1>
+            <h1 style={{fontSize: 50, color: 'red'}}>value = {state}</h1>
             <button
-                style={{background: 'black', color: "white", padding: 15}}
+                style={{background: 'black', color: "yellow", padding: 15}}
                 onClick={increment}
             >
                 incremernt
